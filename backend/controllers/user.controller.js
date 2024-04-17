1534b7e623fb4d94896628544f7d5a1731ace79e
Princess Forson <pribeson121@gmail.com>
2024-04-17 11:23:45 -0700

    Logout worked on

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,13 +1,32 @@
 import User from '../models/userModels.js';
 import bcrypt from 'bcryptjs';
 
+
 // Register new user
 export const register = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, email, password, confirmPassword } = req.body;
+
+    // Check if confirmPassword field matches password
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: 'Passwords do not match' });
+    }
+
+    // Check if user with the same email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User with this email already exists' });
+    }
+
+    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username, password: hashedPassword });
+
+    // Create new user instance
+    const user = new User({ username, email, password: hashedPassword });
+
+    // Save user to database
     await user.save();
+
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
     console.error(error);
@@ -28,8 +47,8 @@ export const login = async (req, res) => {
       return res.status(401).json({ message: 'Incorrect password' });
     }
     // Store user in session 
-    req.session.user = user; 
-    res.json({ message: 'Login successful', user });
+    // req.session.user = user; 
+    res.json({ message: 'Login successful' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
